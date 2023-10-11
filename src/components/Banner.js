af0d468760df317cf924d2aec66d3d81547d84dd
saadat8962 <133997098+saadat8962@users.noreply.github.com>
2023-10-11 12:25:15 +0530

    Project upload

---
+++ src/components/Banner.js
@@ -0,0 +1,23 @@
+function Banner() {
+  return (
+    
+  <section className="banner">
+    <div className="container">
+     <div className="banner-top-wrapper">
+      <div className="banner-top-heading">
+        <h1><span>CROMA</span> cosmetic</h1>
+      </div> 
+      <div className="banner-bottom-title">
+        <p>OF CROMA</p>
+        <p>and body care</p> 
+      </div> 
+     </div>
+     <div className="banner-image-wrapper">
+      <img href=""/>
+     </div>
+    </div>
+  </section>
+  );
+}
+
+export default Banner;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+
+function Navbar() {
+  return (
+    
+  <header className="header">
+    <div className="container">
+      <h1 className="project-heading">Croma</h1>
+    </div>
+  </header>
+  );
+}
+
+export default Navbar;
