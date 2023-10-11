0a60c5a10ce4f076af311bfd18ab2e568f7dc626
Saadat Ali Khan <saadatali.khan@galaxyweblinks.in>
2023-10-11 13:21:19 +0530

    update code

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,22 @@
+import BannerImage from '../assets/img/Boba_x_Ice_Cream_Muracoffee_Brown.png';
+
+
 function Banner() {
   return (
     
   <section className="banner">
     <div className="container">
      <div className="banner-top-wrapper">
-      <div className="banner-top-heading">
-        <h1><span>CROMA</span> cosmetic</h1>
-      </div> 
-      <div className="banner-bottom-title">
-        <p>OF CROMA</p>
-        <p>and body care</p> 
-      </div> 
+        <div className="banner-top-heading">
+          <h1><span>CROMA</span> cosmetic</h1>
+        </div> 
+        <div className="banner-bottom-title">
+          <p className="red-title">OF CROMA</p>
+          <p>and body care</p> 
+        </div> 
      </div>
      <div className="banner-image-wrapper">
-      <img href=""/>
+        <img src={BannerImage}/>
      </div>
     </div>
   </section>
