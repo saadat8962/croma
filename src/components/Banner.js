9a18dc4eab9ce879a7f1bd11e0765177b0f6132d
Saadat Ali Khan <saadatali.khan@galaxyweblinks.in>
2023-10-11 17:34:14 +0530

    update images

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
-import bannerImageBrwn from '../assets/img/Boba_x_Ice_Cream_Muracoffee_Brown.png';
-import btnImageBrwn from '../assets/img/Boba_x_Ice_Cream_r1_1L0A6324_portrait_btn.png';
-import bannerImageRed from '../assets/img/Boba_x_Ice_Cream_PikaChurro_banner.png';
-import btnImageRed from '../assets/img//Boba_x_Ice_Cream_r1_1L0A6413_Portrait_btn.png';
 
+import bannerImageBrwn from '../assets/img/Ice_Cream_banner_brown.png';
+import btnImageBrwn from '../assets/img/Ice_Cream_btn_brown.png';
+
+import bannerImageRed from '../assets/img/Ice_Cream_banner_red.png';
+import btnImageRed from '../assets/img/Ice_Cream_btn_red.png';
+
+import bannerImageMango from '../assets/img/Ice_Cream_banner_mango.png';
+import btnImageMango from '../assets/img/Ice_Cream_btn_mango.png';
+
+import bannerImagePink from '../assets/img/Ice_Cream_banner_pink.png';
+import btnImagePink from '../assets/img/Ice_Cream_btn_pink.png';
+
+import bannerImagePurple from '../assets/img/Ice_Cream_banner_purple.png';
+import btnImagePurple from '../assets/img/Ice_Cream_btn_purple.png';
+
+import bannerImageYellow from '../assets/img/Ice_Cream_banner_yellow.png';
+import btnImageYellow from '../assets/img/Ice_Cream_btn_yellow.png';
+
+const imagePaths = {
+  btn1: bannerImageBrwn,
+  btn2: bannerImageRed,
+  btn3: bannerImageMango,
+  btn4: bannerImagePink,
+  btn5: bannerImagePurple,
+  btn6: bannerImageYellow,
+};
 
 function Banner() {
 
   const [currentImage, setCurrentImage] = useState(bannerImageBrwn);
 
- function handleImageRed() 
- {
-  setCurrentImage(bannerImageRed)
- }
-
- function handleImageBrwn() 
- {
-  setCurrentImage(bannerImageBrwn)
- }
+  const handleImageChange = (buttonKey) => {
+    setCurrentImage(imagePaths[buttonKey]);
+  }
 
   return (
     
@@ -37,11 +53,23 @@ function Banner() {
      </div>
     </div>
     <div className='banner-btn-wrapper'>
-      <button onClick={handleImageBrwn} className='btn-1'>
+      <button onClick={() => handleImageChange('btn1')} className='btn-1'>
         <img src={btnImageBrwn}/>
       </button>
-      <button onClick={handleImageRed} className='btn-1'>
+      <button onClick={() => handleImageChange('btn2')} className='btn-1'>
         <img src={btnImageRed}/>
+      </button>
+      <button onClick={() => handleImageChange('btn3')} className='btn-1'>
+        <img src={btnImageMango}/>
+      </button>
+      <button onClick={() => handleImageChange('btn4')} className='btn-1'>
+        <img src={btnImagePink}/>
+      </button>
+      <button onClick={() => handleImageChange('btn5')} className='btn-1'>
+        <img src={btnImagePurple}/>
+      </button>
+      <button onClick={() => handleImageChange('btn6')} className='btn-1'>
+        <img src={btnImageYellow}/>
       </button>
     </div>
   </section>
